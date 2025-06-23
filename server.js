95d237708426b6b3afa031946ef8b269281b2fd9
aritlhq <aritlhq@gmail.com>
2025-06-23 20:50:52 +0700

    feat: add initial Prisma schema and Express server setup
    Add User model to Prisma schema and configure basic Express server with auth routes

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+// Import Routes
+import authRoutes from './src/routes/authRoutes.js';
+import pageRoutes from './src/routes/pageRoutes.js';
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// EJS Setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+// Middleware
+app.use(express.json()); // To parse JSON bodies
+app.use(express.urlencoded({extended: true})); // To parse URL-encoded bodies
+app.use(cookieParser()); // To parse cookies
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/', pageRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
