614f8384e36f48a036ed6b9af08637d16b2945e2
aritlhq <aritlhq@gmail.com>
2025-06-24 18:54:01 +0700

    feat: implement Prisma integration for dashboard todos
    Refactor getDashboardPage to fetch todos from database using Prisma and handle errors

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,17 +1,43 @@
+import {PrismaClient} from '../../generated/prisma/index.js';
+
+const prisma = new PrismaClient();
+
 export const getHomePage = (req, res) => {
     // The 'user' object is attached by the 'addUserToLocals' middleware
-    res.render('index', { user: res.locals.user || null });
+    res.render('index', {user: res.locals.user || null});
 };
 
 export const getLoginPage = (req, res) => {
-    res.render('login', { error: null, user: null });
+    res.render('login', {error: null, user: null});
 };
 
 export const getRegisterPage = (req, res) => {
-    res.render('register', { error: null, user: null });
+    res.render('register', {error: null, user: null});
 };
 
-export const getDashboardPage = (req, res) => {
-    // The 'user' object is attached by the 'protect' middleware
-    res.render('dashboard', { user: req.user });
-};
+// export const getDashboardPage = (req, res) => {
+//     // The 'user' object is attached by the 'protect' middleware
+//     res.render('dashboard', { user: req.user });
+// };
+
+export const getDashboardPage = async (req, res) => {
+    try {
+        const todos = await prisma.todo.findMany({
+            where: {
+                authorId: req.user.id,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
+
+        res.render('dashboard', { user: req.user, todos, error: null });
+    } catch (error) {
+        console.error(error);
+        res.render('dashboard', {
+            user: req.user,
+            todos: [],
+            error: 'Could not fetch your todos.',
+        });
+    }
+}
