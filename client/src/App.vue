1d648c7743dfbdf519226864f7793598b1b6b204
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-04 06:49:11 +0700

    feat: Add App.vue layout with Navbar and router-view

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app-layout">
+    <Navbar/>
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<style>
+main {
+  padding: 1rem;
+}
+</style>
