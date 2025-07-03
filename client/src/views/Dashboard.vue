aadbd9b293b2840d6ad3c60bb83d9c7886c39b55
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-04 06:45:47 +0700

    feat: Add initial Dashboard view

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <div v-if="auth.isAuthenticated && auth.currentUser">
+      <h2>Welcome, {{ auth.currentUser.name }}!</h2>
+      <p>This is a protected page. Only logged-in users can see this.</p>
+      <p>Your email is: {{ auth.currentUser.email }}</p>
+    </div>
+    <div v-else>
+      <p>Loading user data...</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {useAuthStore} from '../stores/auth';
+
+const auth = useAuthStore();
+</script>
