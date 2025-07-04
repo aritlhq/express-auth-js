a1df12e906ad56da4671167012443aeb246d9543
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-04 08:44:18 +0700

    feat: Add AppFooter component

---
+++ components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>© {{ new Date().getFullYear() }} - My Auth App migrated to Nuxt 3</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 1.5rem;
+  margin-top: 2rem;
+  color: #666;
+  border-top: 1px solid #e2e8f0;
+}
+</style>
