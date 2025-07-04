457e4bf02ac3e062e90033c9e426b18be2f0d6a1
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-04 08:44:23 +0700

    feat: Add AppHeader component

---
+++ components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+const {user, logout} = useAuth();
+const router = useRouter();
+
+const handleLogout = async () => {
+  await logout();
+  router.push('/login');
+};
+</script>
+
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <template v-if="user">
+      <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+      <button @click="handleLogout">Logout</button>
+    </template>
+    <template v-else>
+      <NuxtLink to="/login">Login</NuxtLink>
+      <NuxtLink to="/register">Register</NuxtLink>
+    </template>
+  </nav>
+</template>
