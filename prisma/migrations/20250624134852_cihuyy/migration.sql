7fe0c9a63c9f488b9f0e4426dde1966e6393f3a7
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-04 15:34:51 +0700

    fix: prisma - Corrected typo in column name

--- prisma/migrations/20250624134852_cihuyy/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `passwordResetTokeExpires` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `passwordResetTokeExpires`,
-    ADD COLUMN `passwordResetTokenExpires` DATETIME(3) NULL;
