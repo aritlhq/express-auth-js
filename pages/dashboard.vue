dc6cc8ab6ac9348a61b74833faf9c64bc419df2b
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-04 08:45:11 +0700

    feat: Add dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,18 @@
+<script setup>
+definePageMeta({
+  middleware: 'auth-middleware',
+});
+
+const {user} = useAuth();
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <div v-if="user">
+      <h2>Welcome, {{ user.name }}!</h2>
+      <p>This is a protected page. Only logged-in users can see this.</p>
+      <p>Your email is: {{ user.email }}</p>
+    </div>
+  </div>
+</template>
